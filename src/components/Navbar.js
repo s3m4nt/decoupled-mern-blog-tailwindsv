eeb33e85d52a2e712de680e996fa790dfe7342a4
Brian Young <berkshirehunt@gmail.com>
2021-07-02 16:55:22 -0700

    Create React, layout, Tailwind

---
+++ src/components/Navbar.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+
+const Navbar = () => {
+    return (
+
+        <nav className="flex justify-between item-center h-16 bg-white text-black relative shadow-sm font-mono mt-10" role="navigation">
+            <Link to='/' className="pl-8">
+            EGG
+            </Link>
+            <div className='px-4 cursor-pointer md:hidden'>
+            <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd" /></svg>
+
+            </div>
+            <div className="pr-8 md:block hidden">
+                <Link className="p-4" to="/">Home</Link>
+                <Link className="p-4" to="/Menu">Menu</Link>
+                <Link className="p-4" to="/About">About</Link>
+                <Link className="p-4" to="/Contact">Contact</Link>
+            </div>
+
+
+        </nav>
+
+    )
+}
+
+export default Navbar
