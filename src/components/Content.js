b54d2da66566ff75c99d5706eface5dee00ca092
Brian Young <berkshirehunt@gmail.com>
2021-07-05 13:27:13 -0700

    style v3

--- src/components/Content.js
+++ src/components/Content.js
@@ -23,16 +23,24 @@ function Content() {
     }, [])
       
 const mapData = data.map((blog, idx) => {
-    return <div key={idx} className="mx-12 pt-4">
+    return <div key={idx} className="mx-12 pt-4 text-left">
         <hr className="border-4 my-5" />
     <button>
         <Link to={{
         pathname:"./single",
         state: blog
         }}>
-        <span className="lg:text-4xl md:text-3xl sm:text-3xl font-black">{blog.name}</span>
+        <span className="lg:text-4xl md:text-3xl sm:text-3xl font-black">
+
+            {blog.name}
+
+        </span>
         </Link>
-        <Link className="mt-12 py-6 px-10 bg-yellow-500 rounded-full text-3xl hover:bg-yellow-300 transition duration-300 ease-in-out flex intems-center animate-bounce">
+        <Link to={{
+        pathname:"./single",
+        state: blog
+        }}
+        className="flex justify-center max-w-xs mt-12 py-6 px-10 bg-yellow-500 rounded-full text-3xl hover:bg-yellow-300 transition duration-300 ease-in-out animate-bounce">
             View BLOG.
             <svg className="w-6 h-6 ml-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 20H5a2 2 0 01-2-2V6a2 2 0 012-2h10a2 2 0 012 2v1m2 13a2 2 0 01-2-2V7m2 13a2 2 0 002-2V9a2 2 0 00-2-2h-2m-4-3H9M7 16h6M7 8h6v4H7V8z" /></svg>
         </Link>
