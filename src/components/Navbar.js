371aa1d5335e5d1da03d4fa19262b0902e927a62
Brian Young <berkshirehunt@gmail.com>
2021-07-05 12:26:59 -0700

    styling 2

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,10 +7,10 @@ const Navbar = () => {
     <>
         <nav className="bg-yellow-300 flex justify-between item-center h-16 bg-white text-black relative shadow-md font-mono pt-10 pb-14" role="navigation">
             <Link to='/' className="pl-8 text-2xl font-bold ml-8">
-            MERN Stack BLOG
+            MERN Stack BLOG.
             </Link>
             <div className='px-4 cursor-pointer md:hidden'>
-            <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd" /></svg>
+                <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd" /></svg>
             </div>
             <div className="pr-8 md:block hidden">
                 <Link className="p-4 hover:underline" to="/">BLOG.</Link> <span className="text-black">|</span>
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,14 +1,23 @@
 import React from 'react'
+import {Button} from 'react'
 
 function Contact() {
+
+    const sendMail = () => {
+        const mailto: string =
+          "mailto:mail@gmail.com?subject=Test subject&body=Body content";
+        window.location.href = mailto;
+      }
+
     return (
         <>
             
-            <div className="h-screen flex justify-center items-top">
-                <h1 className="lg:text-9xl md:text-7xl sm:text-5xl text-3xl font-black mb-14 pt-8">CONTACT.</h1>
-
+            <div className="flex justify-center items-top">
+                <h1 className="lg:text-9xl md:text-7xl sm:text-5xl text-3xl font-black mb-12 pt-8">CONTACT.</h1>
             </div>
-
+            <div className="text-center mb-14">
+<a href="mailto:brianyoungdev@protonmail.com">brianyoungdev@protonmail.com</a>
+            </div>
         </>
     )
 }
